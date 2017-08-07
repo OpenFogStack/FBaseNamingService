bf448045e7f8dfb0799f13e553d977f2fb307101
Wm. Keith van der Meulen <wkv@vanmain.com>
2017-08-07 10:50:23 +0200

    Add communication classes

---
+++ src/main/java/namespace/NamespaceSender.java
@@ -0,0 +1,44 @@
+package namespace;
+
+import org.apache.log4j.Logger;
+import org.zeromq.ZMQ;
+
+import communication.AbstractSender;
+import crypto.CryptoProvider;
+import crypto.CryptoProvider.EncryptionAlgorithm;
+import model.JSONable;
+import model.messages.Envelope;
+
+/**
+ * Sends requests to designated receivers.
+ * 
+ * @author jonathanhasenburg
+ *
+ */
+public class NamespaceSender extends AbstractSender {
+
+	private static Logger logger = Logger.getLogger(NamespaceSender.class.getName());
+
+	/**
+	 * Initializes the NamespaceSender, it then can be used without further modifications.
+	 */
+	public NamespaceSender(String address, int port, String secret, EncryptionAlgorithm algorithm) {
+		super(address, port, secret, algorithm, ZMQ.REQ);
+	}
+
+	/**
+	 * Sends an envelope to the specified address.
+	 * 
+	 * @param envelope
+	 * @return the response
+	 */
+	@Override
+	public String send(Envelope envelope) {
+		logger.debug("Sending envelope with keygroup " + envelope.getKeygroupID());
+		sender.sendMore(envelope.getKeygroupID().getID());
+		sender.send(
+				CryptoProvider.encrypt(JSONable.toJSON(envelope.getMessage()), secret, algorithm));
+		return CryptoProvider.decrypt(sender.recvStr(), secret, algorithm);
+	}
+
+}
